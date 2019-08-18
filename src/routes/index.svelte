41e977f4bc523ab98cbdcec4b1a6bd510f82a9f2
v.homic <github@homic.io>
2019-08-18 15:44:44 +0200

    boilerplate instead of 'its working'

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,18 +1,46 @@
 <style>
+	h1, figure, p {
+		text-align: center;
+		margin: 0 auto;
+	}
+
 	h1 {
-		font-size: 2rem;
-		background: yellowgreen;
-    	background: -webkit-linear-gradient(left, orange , yellow, green, cyan, blue, violet);
-    	background: -o-linear-gradient(right, orange, yellow, green, cyan, blue, violet);
-    	background: -moz-linear-gradient(right, orange, yellow, green, cyan, blue, violet);
-    	background: linear-gradient(to right, orange , yellow, green, cyan, blue, violet);
-    	-webkit-background-clip: text;
-		background-clip: text;
-    	-webkit-text-fill-color: transparent;
+		font-size: 2.8em;
+		text-transform: uppercase;
+		font-weight: 700;
+		margin: 0 0 0.5em 0;
+	}
+
+	figure {
+		margin: 0 0 1em 0;
+	}
+
+	img {
+		width: 100%;
+		max-width: 400px;
+		margin: 0 0 1em 0;
+	}
+
+	p {
+		margin: 1em auto;
+	}
+
+	@media (min-width: 480px) {
+		h1 {
+			font-size: 4em;
+		}
 	}
 </style>
 
 <svelte:head>
-	<title>Yay</title>
+	<title>Sapper project template</title>
 </svelte:head>
-<h1>It's working!</h1>
+
+<h1>Great success!</h1>
+
+<figure>
+	<img alt='Borat' src='great-success.png'>
+	<figcaption>HIGH FIVE!</figcaption>
+</figure>
+
+<p><strong>Try editing this file (src/routes/index.svelte) to test live reloading.</strong></p>
